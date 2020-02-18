8017033d18f1980a250e06d7352e815f859a0470
Daniel Wanelle <danielwanelle@gmail.com>
2020-02-18 00:21:34 -0300

    Retirado wp-cisurg

--- wp-cisurg/ead/wp-content/plugins/bigbluebutton/admin/partials/bigbluebutton-recordable-metabox-display.php
+++
@@ -1,4 +0,0 @@
-<label><?php esc_html_e( 'Recordable', 'bigbluebutton' ); ?>: <input name="bbb-room-recordable" type="checkbox" value="checked"
-<?php if ( 'true' === $existing_value) { ?>
-	checked
-<?php } ?>></label>
--- wp-cisurg/ead/wp-content/plugins/wplms-mycred-addon/README.md
+++
@@ -1,4 +0,0 @@
-wplms-mycred-addon
-==================
-
-WPLMS MyCred add on Plugin. This plugin provides integration between MyCred and WPLMS Learning Management system for WordPress.
